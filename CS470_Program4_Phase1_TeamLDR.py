e23e9955aa424ae866d1067b512564221f5482b6
LugwellDR <justin.m.shaner@gmail.com>
2017-04-14 13:40:01 -0700

    Add files via upload
    This is more or less a placeholder Python file. We can just add to it or replace it when needed.

---
+++ CS470_Program4_Phase1_TeamLDR.py
@@ -0,0 +1,10 @@
+# Created By:
+# Justin M. Shaner
+# John Bassler
+# Kieth Lara
+# CS470 - Project 4: Halma Phase 1
+# Started: April 14, 2017
+# Completed: ------
+#
+# Reminder: We will be using Python 3.6.
+#####################################################################
